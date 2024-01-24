8af4d5b7f27c4ebdad9f4ef3fdac0ee8eff57103
lm83680 <qcter@icloud.com>
2024-01-24 14:50:02 +0800

    init

--- README.md
+++ README.md
@@ -9,3 +9,11 @@ create time : January 24, 2024 at 00:26 / 2024年1月24日00点26分
 开源协议:[Apache 2.0](https://www.apache.org/licenses/LICENSE-2.0)
 
 引用的依赖包你可以在[pubspec.yaml](https://github.com/lm83680/yolo_luggage_flutter/blob/main/pubspec.yaml)找到。
+
+### 版本信息
+```shell
+Flutter 3.16.8 • channel stable • https://github.com/flutter/flutter.git
+Framework • revision 67457e669f (7 days ago) • 2024-01-16 16:22:29 -0800
+Engine • revision 6e2ea58a5c
+Tools • Dart 3.2.5 • DevTools 2.28.5
+```
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:yolo_luggage/home/index.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +12,24 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return AdaptiveTheme(
+        light: ThemeData(
+          brightness: Brightness.light,
         ),
-      ),
-    );
+        dark: ThemeData(
+          brightness: Brightness.dark,
+        ),
+        initial: AdaptiveThemeMode.light,
+        builder: (theme, darkTheme) => GestureDetector(
+              onTap: () {
+                FocusScopeNode currentFocus = FocusScope.of(context);
+                currentFocus.focusedChild?.unfocus();
+              },
+              child: GetMaterialApp(
+                theme: theme,
+                darkTheme: darkTheme,
+                home: const HomePage(),
+              ),
+            ));
   }
 }
